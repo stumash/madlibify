fb2d88a5a17125eedfe1570eb8f9506f5d642383
stumash <stumash1@gmail.com>
2018-01-27 18:46:27 -0500

    spaces not tabs!

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,14 @@
 $madlibify_button = null;
+$wordset_button = null;
+
 document.addEventListener("DOMContentLoaded", () => {
     $madlibify_button = $("#js-madlibify-btn")
+    $wordset_button = $("#js-wordset-btn")
+
     $madlibify_button.on("click", () => {
         chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
             console.log("fuck you");
             chrome.tabs.sendMessage(tabs[0].id, "madlibify_button_pressed")
-        });
+        })
     })
 })
